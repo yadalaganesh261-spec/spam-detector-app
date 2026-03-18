93c16335ddd34d7bcd40c5c7c14ba0cf67ad7807
yadalaganesh261-spec <yadalaganesh261@gmail.com>
2026-03-19 02:05:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 
 # Title
-st.title("Ganesh Spam Message Detector 📩")
+st.title("ganesh Spam Message Detector 📩")
 
 # Input box
 message = st.text_input("Enter your message")
